7f5eca880b19e29628d438951bb9461f4d0cb001
Alan Soares <alanrsoars@gmail.com>
2019-03-12 09:11:13 +1300

    add inital set of dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./ui/compounds/NavBar";
+import Footer from "./ui/compounds/Footer";
+import Home from "./ui/pages/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <NavBar />
+        <Home />
+        <Footer />
+      </>
     );
   }
 }
---
+++ src/ui/layouts/Public.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export default function Public(props: Props) {
+  return <div />;
+}
